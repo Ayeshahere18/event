88eae40c1cf46e00571f72349b6f25761e4f45f2
Ayeshahere18 <ayeshashakeelmalik18@gmail.com>
2022-08-02 22:22:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+function myFunction() {
+    document.getElementById("some1").style.height = "50vh";
+  }
+  function myout() {
+    document.getElementById("some1").style.height = "40vh";
+  }
+  function mystyle() {
+    document.getElementById("some2").style.height = "40vh";
+  }
+  function mystyle1() {
+    document.getElementById("some2").style.height = "50vh";
+  }
+  function myboder() {
+    document.getElementById("random").style.border="1px solid blue";
+  }
+  function myboder1() {
+    document.getElementById("random").style.border="0px";
+  }
+  function mycolor() {
+    document.getElementById("random").style.border="5px solid red";
+  }
+  function mycolor1() {
+    document.getElementById("random").style.border="0px";
+  }
+  function myadd() {
+    document.getElementById("check").style.border="5px solid green";
+  }
+  function myadd1() {
+    document.getElementById("check").style.border="0px";
+  }
+  function myrandom() {
+    document.getElementById("check1").style.border="5px solid yellow";
+  }
+  function myrandom1() {
+    document.getElementById("check1").style.border="0px";
+  }
+  function myfashion() {
+    document.getElementById("check2").style.border="5px solid orange";
+  }
+  function myfashion1() {
+    document.getElementById("check2").style.border="0px";
+  }
+  function mytext() {
+    document.getElementById("random").style.border="1px solid white";
+  }
+
+  
+  
